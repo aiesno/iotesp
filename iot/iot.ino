3e92017ddccdf30031e8f8366a529ea8ccc50d8e
aiesno <130806489+aiesno@users.noreply.github.com>
2023-08-16 15:33:01 +0800

    runner cepat

--- iot/iot.ino
+++ iot/iot.ino
@@ -1,1 +1,17 @@
+#define BLYNK_TEMPLATE_ID           "TMPL66lCGWqRq"
+#define BLYNK_DEVICE_NAME           "IotESP32"
+#define BLYNK_AUTH_TOKEN            "2KLiaOI-L0uIbjeS0wmDIgUSZP96AJcj"
 
+char auth[] = BLYNK_AUTH_TOKEN;
+
+char ssid[] = "cuba";
+char pass[] = "cubabarncahbaru";
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(13,OUTPUT);
+  Timer1.setInterval(1000, Timer1_TimerEvent);
+}
+
+void loop() {
+}
