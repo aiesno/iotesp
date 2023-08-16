cf1bb83aa8f4428842a88c028f856eede3018300
aiesno <130806489+aiesno@users.noreply.github.com>
2023-08-16 15:11:21 +0800

    iot.ino

--- iot.ino
+++ iot.ino
@@ -1,13 +1,6 @@
-#include <ESP32Servo.h>
-#include <MakerKit.h>
-
 #define BLYNK_TEMPLATE_ID           "TMPL66lCGWqRq"
 #define BLYNK_DEVICE_NAME           "IotESP32"
 #define BLYNK_AUTH_TOKEN            "2KLiaOI-L0uIbjeS0wmDIgUSZP96AJcj"
-
-
-#define BLYNK_PRINT Serial
-
 
 #include <WiFi.h>
 #include <WiFiClient.h>
@@ -19,7 +12,6 @@ char pass[] = "cubabarncahbaru";
 
 void setup() {
   Serial.begin(9600);
-  Blynk.begin(auth, ssid, pass);
   pinMode(13,OUTPUT);
   servo_23.attach(23);
   Timer1.setInterval(1000, Timer1_TimerEvent);
